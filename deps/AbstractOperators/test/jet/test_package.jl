e76ed7f3b6b8cdd2b3317f63ca94106b8e0b9207
Tamas Hakkel <hakkelt@gmail.com>
2026-09-21 08:52:05 +0200

    chore(AbstractOperators): prune what MRT does not ship

--- deps/AbstractOperators/test/jet/test_package.jl
+++
@@ -1,5 +0,0 @@
-# Standalone: julia --project=test test/jet/test_package.jl
-@testitem "JET test_package" tags = [:jet, :base] begin
-    using JET, AbstractOperators
-    JET.test_package(AbstractOperators; target_modules = (AbstractOperators,))
-end
--- deps/AbstractOperators/test/wavelets/test_quality.jl
+++
@@ -1,4 +0,0 @@
-@testitem "Aqua" tags = [:quality, :wavelet] begin
-    using Aqua, WaveletOperators
-    Aqua.test_all(WaveletOperators; persistent_tasks = VERSION >= v"1.11")
-end
