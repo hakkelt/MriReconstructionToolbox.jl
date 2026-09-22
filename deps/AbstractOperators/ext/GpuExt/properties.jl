7b64f86d4922f15dc7dd060f4897821979cb7d57
Tamas Hakkel <hakkelt@gmail.com>
2026-09-22 10:30:52 +0200

    chore(AbstractOperators): prune what MRT does not ship

--- deps/AbstractOperators/ext/GpuExt/properties.jl
+++
@@ -1,4 +0,0 @@
-array_type_display_string(::Type{<:AbstractGPUArray}) = "ᵍᵖᵘ"
-
-_should_thread(::AbstractGPUArray) = false
-_should_thread(::Type{<:AbstractGPUArray}) = false
--- deps/AbstractOperators/test/nfft/test_quality.jl
+++
@@ -1,4 +0,0 @@
-@testitem "Aqua" tags = [:quality, :nfft] begin
-    using Aqua, NFFTOperators
-    Aqua.test_all(NFFTOperators; persistent_tasks = VERSION >= v"1.11")
-end
